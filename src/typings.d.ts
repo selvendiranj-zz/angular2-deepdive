3468faf8fdd456be19643e67415af154a548e083
selvendiranj <selvendiranj@gmail.com>
2017-03-30 19:59:00 +0530

    formatting changes

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,15 +2,17 @@
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 // tslint:disable
 declare const System: any;
-declare const ENV:string;
+declare const ENV: string;
 // google code-prettify
-declare const PR:any;
+declare const PR: any;
 
 // declare const require:any;
 // declare const global:any;
 
-declare module jasmine {
-  interface Matchers {
-    toHaveCssClass(expected: any): boolean;
-  }
+declare module jasmine
+{
+    interface Matchers
+    {
+        toHaveCssClass(expected: any): boolean;
+    }
 }
